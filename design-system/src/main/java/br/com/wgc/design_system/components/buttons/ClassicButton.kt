73c79daee117f785f9e09646b86956e7f4709975
Gabriel Alves Do Carmo <104175823+GabrielAlvesDoCarmo@users.noreply.github.com>
2025-10-17 14:06:22 -0300

    feat(new template): Finish template loginscreen - New components checkbox

--- design-system/src/main/java/br/com/wgc/design_system/components/buttons/ClassicButton.kt
+++ design-system/src/main/java/br/com/wgc/design_system/components/buttons/ClassicButton.kt
@@ -1,14 +1,17 @@
 package br.com.wgc.design_system.components.buttons
 
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun ClassicButton(
@@ -18,9 +21,9 @@ fun ClassicButton(
     textButton: String = "Button"
 ) {
     ElevatedButton(
-        modifier = modifier.fillMaxWidth(),
+        modifier = modifier.fillMaxWidth().heightIn(56.dp),
         onClick = { onClick() },
-        colors = ButtonColors(
+        colors = ButtonDefaults.elevatedButtonColors(
             containerColor = MaterialTheme.colorScheme.primary,
             contentColor = MaterialTheme.colorScheme.onPrimary,
             disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
@@ -29,7 +32,7 @@ fun ClassicButton(
         enabled = isEnabled,
         shape = RoundedCornerShape(10),
         content = {
-            Text(text = textButton)
+            Text(text = textButton, fontSize = 14.sp)
         },
     )
 }
--- design-system/src/main/java/br/com/wgc/design_system/components/buttons/SecondaryClassicButton.kt
+++ design-system/src/main/java/br/com/wgc/design_system/components/buttons/SecondaryClassicButton.kt
@@ -2,29 +2,30 @@ package br.com.wgc.design_system.components.buttons
 
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.ButtonColors
-import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SecondaryClassicButton(
    modifier: Modifier = Modifier,
    onClick: () -> Unit = {},
-   color: Color = MaterialTheme.colorScheme.primary,
    textButton: String = "Button"
 ){
-    ElevatedButton(
-        modifier = modifier.fillMaxWidth(),
+    OutlinedButton(
+        modifier = modifier.fillMaxWidth().heightIn(56.dp),
         onClick = { onClick()},
         shape = RoundedCornerShape(15),
-        colors = ButtonColors(
+        colors = ButtonDefaults.elevatedButtonColors(
             containerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.2f),
             contentColor = MaterialTheme.colorScheme.secondary,
             disabledContainerColor = MaterialTheme.colorScheme.primary.copy(alpha = 0.5f),
@@ -32,11 +33,10 @@ fun SecondaryClassicButton(
         ),
         border = BorderStroke(
             width = Dp(value = 2.0f),
-            color = color
+            color = MaterialTheme.colorScheme.primary
         )
     ){
-        Text(text = textButton)
-
+        Text(text = textButton, fontSize = 14.sp)
     }
 }
 
