d2e122098ead69c6d0eb1a25384d53bba3fc22ca
Gabriel do Carmo <gabriel.desenvolvedor.97@gmail.com>
2026-09-05 00:23:20 -0300

    feat(core-ds): add WgcCoreDsMotion and WgcCoreDsElevation enterprise design tokens

---
+++ core-ds/src/main/java/br/com/wgc/core_ds/WgcCoreDsElevation.kt
@@ -0,0 +1,18 @@
+package br.com.wgc.core_ds
+
+object WgcCoreDsElevation {
+    /** 0dp - Sem elevação */
+    const val level0 = 0.0
+
+    /** 1dp - Elevação sutil (cards) */
+    const val level1 = 1.0
+
+    /** 3dp - Elevação média (botões elevados) */
+    const val level3 = 3.0
+
+    /** 6dp - Elevação alta (menus suspensos) */
+    const val level6 = 6.0
+
+    /** 8dp - Elevação máxima (modais, diálogos) */
+    const val level8 = 8.0
+}
---
+++ core-ds/src/main/java/br/com/wgc/core_ds/WgcCoreDsMotion.kt
@@ -0,0 +1,15 @@
+package br.com.wgc.core_ds
+
+object WgcCoreDsMotion {
+    /** 100ms - Duração muito rápida */
+    const val durationFast100 = 100
+
+    /** 200ms - Duração rápida */
+    const val durationNormal200 = 200
+
+    /** 300ms - Duração padrão */
+    const val durationStandard300 = 300
+
+    /** 500ms - Duração lenta / ênfase */
+    const val durationSlow500 = 500
+}
