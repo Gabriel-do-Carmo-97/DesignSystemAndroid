5aab45f260bc2d272bc34a39bd35f48efaf9ba6e
Gabriel do Carmo <gabriel.desenvolvedor.97@gmail.com>
2026-09-04 21:09:31 -0300

    feat(ci): add Detekt static analysis and static code checks

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,4 +4,17 @@ plugins {
     alias(libs.plugins.kotlin.android) apply false
     alias(libs.plugins.kotlin.compose) apply false
     alias(libs.plugins.android.library) apply false
+    alias(libs.plugins.detekt) apply false
+}
+
+subprojects {
+    apply(plugin = "io.gitlab.arturbosch.detekt")
+    
+    afterEvaluate {
+        extensions.findByName("detekt")?.let {
+            val detektExt = it as? io.gitlab.arturbosch.detekt.extensions.DetektExtension
+            detektExt?.buildUponDefaultConfig = true
+            detektExt?.ignoreFailures = true
+        }
+    }
 }
